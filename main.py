d1376c4abf8221fc721c861a66361aa8557fb751
sashanktalakola <sashanktalakola2@gmail.com>
2025-06-05 11:48:32 -0500

    feat: integrate LangChain to support multiple LLM providers

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 import os
 import yaml
 import sys
-from openai import OpenAI
 import subprocess
 from utils import dictToNameSpace
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, HumanMessage
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(script_dir, 'config.yaml')
@@ -15,31 +18,24 @@ with open(config_path, "r") as file:
     config = dictToNameSpace(config)
 
 api_key = getattr(config.api_keys, config.main.provider)
-client = OpenAI(
-    api_key=api_key
+provider = config.main.provider
+model_name = config.main.model_name
+
+llm = init_chat_model(
+    model_provider=provider,
+    model=model_name,
+    api_key=api_key,
+    temperature=1.0
 )
 
 user_query = " ".join(sys.argv[1:])
 messages = [
-
-    {
-        "role": "system",
-        "content": "You are an AI assistant that translates natural language descriptions into appropriate Linux/Unix commands. Respond ONLY with the command itself - no explanations, no markdown formatting. For example, if asked 'list all files', respond only with 'ls'."
-    },
-
-    {
-        "role": "user",
-        "content": user_query
-    }
-
+    SystemMessage(content="You are an AI assistant that translates natural language descriptions into appropriate Linux/Unix commands. Respond ONLY with the command itself - no explanations, no markdown formatting. For example, if asked 'list all files', respond only with 'ls'."),
+    HumanMessage(content=user_query)
 ]
 
-response = client.responses.create(
-    model=config.main.model_name,
-    input=messages
-)
-
-command = response.output_text
+response = llm.invoke(messages)
+command = response.content
 
 print(f"\033[1;32mCommand: {command}\033[0m")
 confirm = input("Execute this command? [y/N]: ").lower().strip()
